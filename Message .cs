4f88e65995b1159cdffbe60c4be1ea653a02b74a
epdubiso <35310550+epdubiso@users.noreply.github.com>
2018-02-02 13:26:46 -0800

    Add files via upload

---
+++ Message .cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CommunityAssist2017MVC.Controllers;
+
+namespace CommunityAssist2017MVC.Models
+{
+    public class Message
+    {
+    }
+}
---
+++ RegistrationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2017MVC.Models;
+
+namespace CommunityAssist2017MVC.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View(db.PersonAddresses.ToList());
+        }
+
+        public ActionResult Register()
+
+
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "LastName, FirstName,"+
+             "Email, Phone, PlainPassword, Apartment, Street, State, ZipCode" )]NewPerson np)
+        {
+            int result = db.usp_Person(
+                np.LastName,
+                np.FirstName,
+                np.Email,
+                np.Phone,
+                np.PlainPassword,
+                np.Apartment,
+                np.Street,
+                np.State,
+                np.Zipcode);
+
+            if (result != -1)
+
+            { 
+
+                return RedirectToAction("Index");
+            }
+
+
+            return View();
+        }
+
+    }
+}
